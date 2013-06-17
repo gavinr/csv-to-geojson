af364090ee1ad497916ea5684c7b0ae5d5baa640
Gavin Rehkemper <gavreh@gmail.com>
2013-06-16 20:36:17 -0500

    Initial files.

---
+++ js/app.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+	var app = new App("Gavin", "Rehkemper");
+});
+
+var App = makeClass();
+App.prototype.init = function() {
+	$("#csvTextArea").click(function(evt) {
+		if($("#csvTextArea").val() === "Put CSV here.") {
+			$("#csvTextArea").val("");
+		}
+	})
+	$("#convertButton").click($.proxy(function(evt) {
+		var csvObject = CSVToArray($("#csvTextArea").val());
+		var util = new Util();
+
+		var latName = util.getColName(csvObject, ['lat', 'Lat', 'LAT', 'latitude', 'Latitude', 'LATITUDE']);
+		var lonName = util.getColName(csvObject, ['lng', 'Lng', 'LNG', 'lon', 'Lon', 'LON', 'longitude', 'Longitude', 'LONGITUDE']);
+		console.log("latName:",latName);
+		console.log("lonName:",lonName);
+
+		var massagedData = util.massageData(csvObject);
+		console.log("MassagedData:", massagedData);
+		GeoJSON.parse(massagedData, {
+			Point: [latName, lonName]
+		}, function(geojson) {
+			$("#resultTextArea").show();
+			$("#resultTextArea").val(JSON.stringify(geojson));
+		});
+	}, this));
+};
+
+var Util = makeClass();
+
+Util.prototype.massageData = function(data) {
+	if (data && data.length > 2) {
+		var returnData = [];
+		var dataNoHeader = $.extend(true, [], data);
+		dataNoHeader.splice(0, 1);
+		dataNoHeader.forEach(function(item) {
+			console.log(item);
+			var returnItem = {}, i = 0;
+			data[0].forEach(function(columnName) {
+				returnItem[columnName] = item[i];
+				i++;
+			}, this);
+			returnData.push(returnItem);
+		}, this);
+		return returnData;
+	}
+	return null;
+};
+
+Util.prototype.getColName = function(data, possibleColumnNames) {
+	if (data && data.length > 2) {
+		for (var i = 0; i < data[0].length; i++) {
+			if (possibleColumnNames.indexOf(data[0][i]) !== -1) {
+				return data[0][i];
+			}
+		}
+		// data[0].forEach(function(columnName) {
+		// 	if (possibleColumnNames.indexOf(columnName) !== -1) {
+		// 		return columnName;
+		// 	}
+		// });
+	}
+}
---
+++ readme.md
@@ -0,0 +1,22 @@
+CSV To GeoJSON
+================
+
+Need to convert some CSV points to GeoJSON? This is a simple project that helps you do that.
+
+Example/Production site: http://csv.togeojson.com/
+
+Credit
+------
+
+* Using CSV to JSON function from http://stackoverflow.com/a/1293163/2039 (License http://creativecommons.org/licenses/by-sa/3.0/)
+* Using JSON to GeoJSON package from [caseypt](https://github.com/caseypt) https://github.com/caseypt/geojson.js (licnese unknown)
+
+
+Feedback
+--------
+
+File bug reports here on GitHub or contact [Gavin Rehkemper](http://github.com/gavreh) [(gavinrehkemper @ twitter)](http://twitter.com/gavinrehkemper) for anything else.
+
+License
+-------
+http://creativecommons.org/licenses/by-sa/3.0/
