4fbfd05a60633330712ef34d27fd72733db5fd31
Vinicius Gravina da Rocha <vinicius.darocha@gmail.com>
2015-01-15 22:58:49 -0200

    * removed makeClass overhead
    * isolated the functionality of csvToGeoJSON by:
     1 - creating function csvToGeoJSON inside CsvToJson.js
     2 - moved all plumbing the was in app.js to this function
    
    * added error checking inside csvToGeoJSON() in case the correct header
    is not found

--- js/app.js
+++ js/app.js
@@ -1,103 +1,42 @@
 $(document).ready(function() {
-	var app = new App("Gavin", "Rehkemper");
-});
-
-var App = makeClass();
-App.prototype.init = function() {
 	$("#csvTextArea").click(function(evt) {
 		if ($("#csvTextArea").val() === "Put CSV here.") {
 			$("#csvTextArea").val("");
 		}
 	})
+	
 	$("#convertButton").click($.proxy(function(evt) {
-		var csvObject = CSVToArray($("#csvTextArea").val());
-		var util = new Util();
+			
+		var geojson = csvToGeoJSON($("#csvTextArea").val());
+		
+		$("#resultTextArea").show();
 
-		var latName = util.getColName(csvObject, ['lat', 'Lat', 'LAT', 'latitude', 'Latitude', 'LATITUDE']);
-		var lonName = util.getColName(csvObject, ['lng', 'Lng', 'LNG', 'lon', 'Lon', 'LON', 'longitude', 'Longitude', 'LONGITUDE']);
+		$("#resultTextArea").val(JSON.stringify(geojson));
 
-		var massagedData = util.massageData(csvObject);
-		// take that data and cast the lat and lon columns to numbers using parseFloat:
-		massagedData = util.latLonColumnsToNumbers(massagedData, latName, lonName);
-
-		GeoJSON.parse(massagedData, {
-			Point: [latName, lonName]
-		}, function(geojson) {
-			$("#resultTextArea").show();
-
-			$("#resultTextArea").val(JSON.stringify(geojson));
-
-			$.ajax({
-				url: 'https://api.github.com/gists',
-				headers: {
-					"User-Agent": "csv-to-geojson",
-					"Origin": "http://togeojson.com"
-				},
-				type: "POST",
-				cache: false,
-				processData: false,
-				data: JSON.stringify({
-					"description": "GEOJSON created by http://csv.togeojson.com",
-					"public": true,
-					"files": {
-						"csv-to-geojson.geojson": {
-							"content": JSON.stringify(geojson)
-						}
+		$.ajax({
+			url: 'https://api.github.com/gists',
+			headers: {
+				"User-Agent": "csv-to-geojson",
+				"Origin": "http://togeojson.com"
+			},
+			type: "POST",
+			cache: false,
+			processData: false,
+			data: JSON.stringify({
+				"description": "GEOJSON created by http://csv.togeojson.com",
+				"public": true,
+				"files": {
+					"csv-to-geojson.geojson": {
+						"content": JSON.stringify(geojson)
 					}
-				})
-
-			}).done(function(msg) {
-				$("#gistLink").attr("href", msg.html_url);
-				$("#gistLinkContainer").show();
-			});
+				}
+			})
+		})
+		.done(function(msg) {
+			$("#gistLink").attr("href", msg.html_url);
+			$("#gistLinkContainer").show();
 		});
+	
 	}, this));
-
-};
-
-var Util = makeClass();
-
-Util.prototype.massageData = function(data) {
-	if (data && data.length > 2) {
-		var returnData = [];
-		var dataNoHeader = $.extend(true, [], data);
-		dataNoHeader.splice(0, 1);
-		dataNoHeader.forEach(function(item) {
-			var returnItem = {}, i = 0;
-			data[0].forEach(function(columnName) {
-				returnItem[columnName] = item[i];
-				i++;
-			}, this);
-			returnData.push(returnItem);
-		}, this);
-		return returnData;
-	}
-	return null;
-};
-
-Util.prototype.latLonColumnsToNumbers = function(data, latName, lonName) {
-	data.forEach(function(item) {
-		if(item.hasOwnProperty(latName)) {
-			item[latName] = parseFloat(item[latName]);
-		};
-		if(item.hasOwnProperty(lonName)) {
-			item[lonName] = parseFloat(item[lonName]);
-		};
-	});
-	return data;
-}
-
-Util.prototype.getColName = function(data, possibleColumnNames) {
-	if (data && data.length > 2) {
-		for (var i = 0; i < data[0].length; i++) {
-			if (possibleColumnNames.indexOf(data[0][i]) !== -1) {
-				return data[0][i];
-			}
-		}
-		// data[0].forEach(function(columnName) {
-		// 	if (possibleColumnNames.indexOf(columnName) !== -1) {
-		// 		return columnName;
-		// 	}
-		// });
-	}
-}
+	
+});
--- js/libs/CsvToJson.js
+++ js/libs/CsvToJson.js
@@ -83,3 +83,65 @@ function CSVToArray(strData, strDelimiter) {
     // Return the parsed data.
     return (arrData);
 }
+
+//Converts from an array of arrays to an array of map,
+//with the keys as the headers and the values as the csv value
+var massageData = function(data) {
+	if (data && data.length > 2) {
+		var returnData = [];
+		var dataNoHeader = $.extend(true, [], data);
+		dataNoHeader.splice(0, 1);
+		dataNoHeader.forEach(function(item) {
+			var returnItem = {}, i = 0;
+			data[0].forEach(function(columnName) {
+				returnItem[columnName] = item[i];
+				i++;
+			}, this);
+			returnData.push(returnItem);
+		}, this);
+		return returnData;
+	}
+	return null;
+};
+
+//just parses lat/long to number
+var latLonColumnsToNumbers = function(data, latName, lonName) {
+	data.forEach(function(item) {
+		if(item.hasOwnProperty(latName)) {
+			item[latName] = parseFloat(item[latName]);
+		};
+		if(item.hasOwnProperty(lonName)) {
+			item[lonName] = parseFloat(item[lonName]);
+		};
+	});
+	return data;
+}
+
+var getColName = function(data, possibleColumnNames) {
+	if (data && data.length > 2) {
+		var header=data[0]
+		for (var i = 0; i < header.length; i++) {
+			if (possibleColumnNames.indexOf(header[i]) !== -1) {
+				return data[0][i];
+			}
+		}
+	}
+}
+
+var csvToGeoJSON = function(str, separator, latitudeHeaderList, longitudeHeaderList){
+	var csvObject = CSVToArray(str, separator);
+	
+	var latName = getColName(csvObject, latitudeHeaderList || 
+													['lat', 'Lat', 'LAT', 'latitude', 'Latitude', 'LATITUDE']);
+	if(!latName) {console.error("Couldn't find Latitude header"); return;}
+		
+	var lonName = getColName(csvObject, longitudeHeaderList || 
+														['lng', 'Lng', 'LNG', 'lon', 'Lon', 'LON', 'longitude', 'Longitude', 'LONGITUDE']);
+	if(!lonName) {console.error("Couldn't find Longitude header"); return;}
+
+	var massagedData = massageData(csvObject);
+	// take that data and cast the lat and lon columns to numbers using parseFloat:
+	massagedData = latLonColumnsToNumbers(massagedData, latName, lonName);
+	
+	return GeoJSON.parse(massagedData, {Point: [latName, lonName]});
+}
